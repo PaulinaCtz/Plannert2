0f4a7b1b696ff4217f9bb70e25169dca2f72d586
PaulinaCtz <paulina.cortez228557@potros.itson.edu.mx>
2023-05-13 19:49:52 -0700

    Usuario primera vez

--- Moviles/app/src/main/java/mx/edu/plannert/Bienvenida2.kt
+++ Moviles/app/src/main/java/mx/edu/plannert/Bienvenida2.kt
@@ -3,8 +3,16 @@ package mx.edu.plannert
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Bienvenida2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- Moviles/app/src/main/java/mx/edu/plannert/MainActivity.kt
+++ Moviles/app/src/main/java/mx/edu/plannert/MainActivity.kt
@@ -16,17 +16,13 @@ import com.google.android.gms.common.api.Api
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
-
-    private lateinit var databaseReference: DatabaseReference
-
-    //private lateinit var auth: FirebaseAuth
     private lateinit var auth: FirebaseAuth
-    private lateinit var client:GoogleSignInClient
-    val LOG_OUT = 234
+    private lateinit var client: GoogleSignInClient
+    private val LOG_OUT = 234
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Plannert)
@@ -48,138 +44,118 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        try{
+        try {
             val options = GoogleSignInOptions.Builder().requestEmail().build()
-            client = GoogleSignIn.getClient(this,options)
+            client = GoogleSignIn.getClient(this, options)
             inicioGoogle.setOnClickListener {
                 val intent = client.signInIntent
-                startActivityForResult(intent,100)
+                startActivityForResult(intent, 100)
             }
-        }catch (e:ApiException){
-
+        } catch (e: ApiException) {
+            // Manejar la excepción
         }
 
-
         botonIS.setOnClickListener {
-
-            if(emailET.text.isNotEmpty() && pswET.text.isNotEmpty() ){
-                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailET.text.toString(),
-                    pswET.text.toString()).addOnCompleteListener {
-                    if(it.isSuccessful){
-                        showHome()
-                    }else{
+            if (emailET.text.isNotEmpty() && pswET.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    emailET.text.toString(),
+                    pswET.text.toString()
+                ).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        UsuariosEstados()
+                    } else {
                         showAlert("Se produjo un error autenticando al usuario.")
                     }
                 }
-            }else{
+            } else {
                 showAlert("Debes llenar todos los campos")
             }
-            /*if (emailET.text.isEmpty() || pswET.text.isEmpty()) {
-                Toast.makeText(this, "Por favor, ingresa tu correo electrónico y contraseña", Toast.LENGTH_SHORT).show()
-            } else {
-                if (emailET.text.isNotEmpty() && pswET.text.isNotEmpty()) {
-                    FirebaseAuth.getInstance().signInWithEmailAndPassword(
-                        emailET.text.toString().trim(),
-                        pswET.text.toString().trim()
-                    ).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            val intent = Intent(this, Introductorio::class.java)
-                            startActivity(intent)
-                            finish()
-                        } else {
-                            // El inicio de sesión falló, mostrar mensaje de error
-                            Toast.makeText(
-                                this,
-                                "Inicio de sesión fallido, verifica la contraseña",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    }
-                }
-            }*/
-
-            /*
-            val email = email_txt.text.toString().trim()
-            val contraseña = contraseña_txt.text.toString().trim()
-
-            if (email.isEmpty() || contraseña.isEmpty()) {
-                Toast.makeText(this, "Por favor, ingresa tu correo electrónico y contraseña", Toast.LENGTH_SHORT).show()
-            } else {
-                // Iniciar sesión con el usuario y la contraseña
-                auth.signInWithEmailAndPassword(email, contraseña)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            // El inicio de sesión fue exitoso, redirigir a la página Introductorio
-                            val intent = Intent(this, Introductorio::class.java)
-                            startActivity(intent)
-                            finish() // Evitar que el usuario regrese a la pantalla de inicio de sesión
-                        } else {
-                            // El inicio de sesión falló, mostrar mensaje de error
-                            Toast.makeText(this, "Inicio de sesión fallido, verifica la contraseña", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-            }*/
         }
 
         necesitoAyuda.setOnClickListener {
             val intent = Intent(this, help::class.java)
             startActivity(intent)
         }
-
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==100){
-            try{
+        if (requestCode == 100) {
+            try {
                 val task = GoogleSignIn.getSignedInAccountFromIntent(data)
                 val account = task.getResult(ApiException::class.java)
-                val credencial = GoogleAuthProvider.getCredential(account.id,null)
-                FirebaseAuth.getInstance().signInWithCredential(credencial).addOnCompleteListener{
-                    if(task.isSuccessful){
-
-                        val i = Intent(this,Inicio::class.java)
-                        i.putExtra("name",FirebaseAuth.getInstance().currentUser?.displayName)
-                        startActivity(i)
-
-                    }else{
-                        Toast.makeText(this,task.exception?.message,Toast.LENGTH_SHORT).show()
+                val credencial = GoogleAuthProvider.getCredential(account.idToken, null)
+                FirebaseAuth.getInstance().signInWithCredential(credencial)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            UsuariosEstados()
+                        } else {
+                            showAlert("Error al iniciar sesión con Google.")
+                        }
                     }
-                }
-            }catch (e:ApiException){
-
+            } catch (e: ApiException) {
+                // Manejar la excepción
             }
-            }
+        }
 
-        if(requestCode==LOG_OUT){
+        if (requestCode == LOG_OUT) {
             signOut()
         }
     }
 
     override fun onStart() {
         super.onStart()
-        if(FirebaseAuth.getInstance().currentUser!=null){
-            val i = Intent(this,Inicio::class.java)
-            i.putExtra("name",FirebaseAuth.getInstance().currentUser?.email)
-            startActivity(i)
+    }
+
+    private fun UsuariosEstados() {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+        if (currentUser != null) {
+            val email = currentUser.email
+
+            // Verificar si el correo existe en la base de datos
+            val databaseReference = FirebaseDatabase.getInstance().getReference("usuarios")
+            val query = databaseReference.orderByChild("email").equalTo(email)
+            query.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        val user = dataSnapshot.children.first().getValue(Usuarios::class.java)
+
+                        if (user != null) {
+                            if (user.usuario != null && user.usuario.isNotEmpty()) {
+                                // El usuario ya tiene el atributo "usuario", redirigir a la página de inicio
+                                startActivity(Intent(this@MainActivity, Inicio::class.java))
+                            } else {
+                                // El usuario no tiene el atributo "usuario", redirigir a la página de introducción
+                                startActivity(Intent(this@MainActivity, Introductorio::class.java))
+                            }
+                        } else {
+                            // El usuario no tiene información en la base de datos, redirigir a la página de introducción
+                            startActivity(Intent(this@MainActivity, Introductorio::class.java))
+                        }
+                    } else {
+                        // El correo no existe en la base de datos, redirigir a la página de introducción
+                        startActivity(Intent(this@MainActivity, Introductorio::class.java))
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    showAlert("Se produjo un error al acceder a la base de datos.")
+                }
+            })
         }
     }
 
-    private fun showAlert (msg:String) {
+    private fun showAlert(msg: String) {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
         builder.setMessage(msg)
-        builder.setPositiveButton( "Aceptar",null)
+        builder.setPositiveButton("Aceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
 
-    private fun showHome(){
-        val homeIntent= Intent(this, Introductorio::class.java)
-        startActivity(homeIntent)
-    }
-
-    private fun signOut(){
+    private fun signOut() {
         client.signOut().addOnCompleteListener(this) {
             Toast.makeText(
                 this,
@@ -188,6 +164,7 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
     }
+
     fun generarPassword(longitud: Int): String {
         val caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
         val random = java.security.SecureRandom()
@@ -199,3 +176,4 @@ class MainActivity : AppCompatActivity() {
         return sb.toString()
     }
 }
+
